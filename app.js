4ca45f33b794110978a8ceb428c57de581fa59d9
cahuayamarcial <cahuaya.marcial@gmail.com>
2019-01-05 16:57:34 -0400

    Compilation of code 😅

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("Hello!");
+/* Basic Types
+ * For programs to be useful, we need to be able to work with some of the simplest units of data:
+ * numbers, strings, structures, boolean values, and the like.
+ * In TypeScript, we support much the same types as you would expect in JavaScript, with a convenient enumeration type thrown in to help things along.
+ */
+// Boolean
+var isDone = false;
+// Number
+var decimal = 6;
+var hex = 0xf00d;
+var dinary = 10;
+var octal = 484;
+// String
+var color = "blue";
+color = "red";
+// Array
+var list = [1, 2, 3];
+// Tuple
+var x;
+x = ["hello", 10]; // OK
+// x = [10, "hello"]; // Error
+// Enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+// Any
+var notSure = 4;
+notSure = "maybe a string instead";
+notSure = false; // okay, definitely a boolean
+// Void
+function warnUser() {
+    console.log("This is my warning message");
+}
+// Null and Undefined
+var u = undefined;
+var n = null;
